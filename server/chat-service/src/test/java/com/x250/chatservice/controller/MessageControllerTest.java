236c88a2d7b032a7343fb3afde9b46babbd4e966
Panagiotis Bellias <belliaspan@gmail.com>
2024-04-22 12:40:23 +0300

    Add unit tests for chat service maven module

--- server/chat-service/src/test/java/com/x250/chatservice/controller/MessageControllerTest.java
+++ server/chat-service/src/test/java/com/x250/chatservice/controller/MessageControllerTest.java
@@ -1,4 +1,31 @@
 package com.x250.chatservice.controller;
 
-public class MessageControllerTest {
+import com.x250.chatservice.service.MessageService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MessageControllerTest {
+
+    @InjectMocks
+    MessageController messageController;
+
+    @Mock
+    MessageService messageService;
+
+    @Test
+    void testConstructor() {
+        MessageController messageController = new MessageController(messageService);
+        Assertions.assertInstanceOf(MessageController.class, messageController);
+    }
+
+    @Test
+    void testGetMessages() {
+        Assertions.assertTrue(messageController.getMessages("room").getBody().isEmpty());
+    }
+
 }
--- server/chat-service/src/test/java/com/x250/chatservice/model/MessageTest.java
+++ server/chat-service/src/test/java/com/x250/chatservice/model/MessageTest.java
@@ -1,4 +1,55 @@
 package com.x250.chatservice.model;
 
-public class MessageTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+@ExtendWith(MockitoExtension.class)
+class MessageTest {
+
+    @Mock
+    Date date;
+
+    @Test
+    void testNoArgsConstructor() {
+        Message message = new Message();
+        Assertions.assertInstanceOf(Message.class, message);
+    }
+
+    @Test
+    void testAllArgsConstructor() {
+        Message message = new Message(0L, "room", "username", "email", "image url", "message", date);
+        Assertions.assertEquals(0L, message.getId());
+        Assertions.assertEquals("room", message.getRoom());
+        Assertions.assertEquals("username", message.getUsername());
+        Assertions.assertEquals("email", message.getEmail());
+        Assertions.assertEquals("image url", message.getImageUrl());
+        Assertions.assertEquals("message", message.getMessage());
+        Assertions.assertInstanceOf(Date.class, message.getCreatedDateTime());
+    }
+
+    @Test
+    void testSetters() {
+        Message message = new Message();
+        message.setId(0L);
+        message.setRoom("room");
+        message.setUsername("username");
+        message.setEmail("email");
+        message.setImageUrl("image url");
+        message.setMessage("message");
+        message.setCreatedDateTime(date);
+
+        Assertions.assertEquals(0L, message.getId());
+        Assertions.assertEquals("room", message.getRoom());
+        Assertions.assertEquals("username", message.getUsername());
+        Assertions.assertEquals("email", message.getEmail());
+        Assertions.assertEquals("image url", message.getImageUrl());
+        Assertions.assertEquals("message", message.getMessage());
+        Assertions.assertInstanceOf(Date.class, message.getCreatedDateTime());
+    }
+
 }
